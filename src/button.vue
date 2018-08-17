56870156fa0caae6b9b2edcd548432ee258f8230
maque28701087 <2012646936@qq.com>
2018-08-17 21:44:58 +0800

    添加 Loading 状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
         <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -25,6 +26,10 @@
 </script>
 
 <style lang="scss">
+    @keyframes spin {
+        0%{ transform: rotate(0deg);}
+        100%{transform: rotate(360deg);}
+    }
     .g-button{display: inline-flex;justify-content: center;align-items: center;font-size: var(--font-size);height:
             var(--button-height);padding: 0 1em;border-radius: var(--button-radius);border: 1px solid var(--border-color);
             background: var(--button-bg); vertical-align: top;
@@ -36,6 +41,9 @@
         &.icon-right{
             > .icon{order: 2;margin-left: .3rem;margin-right: 0;}
             > .content{order: 1;}
+        }
+        .loading{
+            animation: 1.5s spin infinite linear;
         }
     }
 
