c33591d23f16d7f861f10b1242bd8d41a6a7ad47
maque28701087 <2012646936@qq.com>
2018-08-18 10:08:10 +0800

    检测子元素是不是 button

---
+++ src/button-grop.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted(){
+            for(let node of this.$el.children){
+                if(node.nodeName.toLowerCase() !== 'button'){
+                    console.warn('g-button-group 的子元素应该是 g-button')
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .g-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .g-button{
+            border-radius: 0;
+            margin-left: -2px;
+            &:first-child{
+                border-top-left-radius: var(--button-radius);
+                border-bottom-left-radius: var(--button-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--button-radius);
+                border-bottom-right-radius: var(--button-radius);
+            }
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
